96082a5fba8a76b8cb180d926923129d3b8b24f0
MiWyzard <IkhsanKusuma1551@gmail.com>
2024-06-07 22:37:11 +0700

    Develop add todo feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -17,6 +17,7 @@ const TodoItem = ({ todo, toggleCompleted }) => {
         onChange={() => toggleCompleted(todo.id)}
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      <button style={styles.button}>x</button>
     </div>
   )
 }
@@ -35,16 +36,24 @@ const styles = {
   todoItem: {
     border: '2px solid #f4f4f4',
     fontSize: '24px',
-    // Tambahkan styles di bawah ini
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    padding: '0 20px',
   },
-  // Tambahkan styles di bawah ini
   checkbox: {
-    marginRight: '10px',
     height: '18px',
     width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
   },
 }
 
